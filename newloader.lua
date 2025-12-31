9bf89c115c44ad2c262f30e5625da4db46d7b4f0
Chavel <jovancioon@gmail.com>
2025-12-31 12:36:15 +0700

    Create newloader.lua

---
+++ newloader.lua
@@ -0,0 +1,21 @@
+local SUPPORTED_GAMES = {
+    [18794863104] = true,
+    [18199615050] = true,
+    [130565641176498] = true
+}
+
+local currentPlaceId = game.PlaceId
+
+if SUPPORTED_GAMES[currentPlaceId] then
+    local PulseHub = loadstring(game:HttpGet("https://pulsehubb.vercel.app/scripts/verify.lua"))()
+    
+    PulseHub.ShowKeyUI(function(result)
+        if result.script then
+            print("Script loaded:", result.script.name)
+        elseif result.no_script_for_game then
+            print("No script for this game")
+        end
+    end)
+else
+    warn("[PulseHub] This game is not supported. PlaceId:", currentPlaceId)
+end
